0023110cb4c0ac5db1569c248fa183a0053b034e
Marius Merkevicius <marius.merkevicius@shift4.com>
2024-06-10 22:22:42 +0300

    Database log file pulling

--- adb/adb-emu-sql-download.sh
+++ adb/adb-emu-sql-download.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-#
-# !!! Incomplete !!!
-#
+INPUT_DEVICE=$1
+INPUT_PACKAGE=$2
 
-INPUT_PACKAGE=$1
-if [[ -z ${INPUT_PACKAGE} ]] ; then
-    echo "./adb-emu-sql-download.sh [package]"
-    echo "Ex: ./adb-emu-download.sh com.package.app"
-    exit 1
+#set -x
+## Sanity checks
+## Agument assert
+if [ $# -lt 2 ]; then
+    echo "No arguments supplied"
+    echo "Args: [input device] [input package]"
+    echo "Ex: emulator-5554 com.innerpackage1.app"
+    exit 1;
 fi
+
+INNER_PATH_EXT="/sdcard/Download"
+INNER_FILE_DB="database.sqlite"
 #adb shell "run-as ${INPUT_PACKAGE} sh -c \"du databases/database.sqlite \""
-adb -d shell "run-as ${INPUT_PACKAGE} cp databases/database.sqlite /sdcard/Download/"
+adb -s ${INPUT_DEVICE} -d shell "run-as ${INPUT_PACKAGE} cp databases/${INNER_FILE_DB} ${INNER_PATH_EXT}"
+adb pull "${INNER_PATH_EXT}/${INNER_FILE_DB}"
