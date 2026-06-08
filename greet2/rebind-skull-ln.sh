4148afce4afc658fdaedb16ea8b60c5aee5d837f
Marius Merkevicius <merkevicius.marius@gmail.com>
2026-06-08 18:58:33 +0300

    Fix issues on skull binding in linux

--- greet2/rebind-skull-ln.sh
+++ greet2/rebind-skull-ln.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 ## Fetching script location
 ## Source: https://www.baeldung.com/linux/bash-get-location-within-script
-SCRIPT_PATH="${BASH_SOURCE}"
+SCRIPT_PATH="${BASH_SOURCE[0]}"
 while [ -L "${SCRIPT_PATH}" ]; do
   TARGET="$(readlink "${SCRIPT_PATH}")"
   if [[ "${TARGET}" == /* ]]; then
@@ -11,32 +11,29 @@ while [ -L "${SCRIPT_PATH}" ]; do
     SCRIPT_PATH="$(dirname "${SCRIPT_PATH}")/${TARGET}"
   fi
 done
-SCRIPT_PATH=$(dirname ${SCRIPT_PATH})
+SCRIPT_PATH="$(dirname "${SCRIPT_PATH}")"
 
 ## Move to skull folder
-RANDOM_SKULL_LN=random-skull.txt
-REPO_DIR=${SCRIPT_PATH}/ascii-all
-cd $SCRIPT_PATH
+RANDOM_SKULL_LN="random-skull.txt"
+REPO_DIR="${SCRIPT_PATH}/ascii-all"
+cd "${SCRIPT_PATH}" || exit 1
 
 ## Array of skulls
 ## Add all skulls to array from 'find function'
 FILES=()
-find ${REPO_DIR} -name "*.txt" -print0 >tmpfile
-while IFS=  read -r -d $'\0'; do
+while IFS= read -r -d ''; do
   FILES+=("$REPLY")
-done <tmpfile
-rm -f tmpfile
+done < <(find "${REPO_DIR}" -name "*.txt" -print0)
 
 ## Pick random skull from repo
-FILE_LENGTH_INDEX=0
-FILE_LENGTH=${#FILES[@]}
-((FILE_LENGTH_INDEX=${FILE_LENGTH}-1))
-RANDOM_SKULL_NUM=$(shuf -i "0-${FILE_LENGTH_INDEX}" -n 1)
-RANDOM_SKULL=${FILES[${RANDOM_SKULL_NUM}]}
-
-#echo "Random skull: ${RANDOM_SKULL} (${RANDOM_SKULL_NUM}) (${FILE_LENGTH_INDEX})"
+FILE_LENGTH="${#FILES[@]}"
+if (( FILE_LENGTH == 0 )); then
+  echo "No skull files found in ${REPO_DIR}" >&2
+  exit 1
+fi
+RANDOM_SKULL_NUM=$(( RANDOM % FILE_LENGTH ))
+RANDOM_SKULL="${FILES[${RANDOM_SKULL_NUM}]}"
 
 ## Rm old symlink
-rm ${RANDOM_SKULL_LN}
-#echo "Using ${RANDOM_SKULL} -> (${RANDOM_SKULL_LN})"
-ln -s $RANDOM_SKULL ${RANDOM_SKULL_LN}
+rm -f "${RANDOM_SKULL_LN}"
+ln -s "${RANDOM_SKULL}" "${RANDOM_SKULL_LN}"
